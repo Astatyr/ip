9e9316e0ba058b918fc7cb91858c42489e1256a9
Justin Adrian Halim <justin.halim20@gmail.com>
2026-01-30 16:43:16 +0800

    Revert "Level-0, copied from my other repo (I previously forgot to change to the account i listed in the canvas) https://github.com/JustinAdrianHalim/ip"
    This reverts commit 57c14beb7c8a78d1ed2e0105fb3842d6403bf22f.

--- src/main/java/Lilith.java
+++
@@ -1,8 +0,0 @@
-public class Lilith {
-    public static void main(String[] args) {
-        System.out.println("Hello, I'm Lilith!");
-        System.out.println("Would you like a strawberry cake?");
-        System.out.println("--------------------------------------------------------------");
-        System.out.println("Bye. Hope to see you again soon!");
-    }
-}
