3e69f7d1298227890c5a39c2a065d9f110ed47bc
Justin Adrian Halim <justin.halim20@gmail.com>
2026-02-09 02:17:37 +0800

    Cleaning code

--- src/main/java/Lilith.java
+++ src/main/java/Lilith.java
@@ -7,8 +7,10 @@ import java.util.Scanner;
  *Lilith class, main application class.
  */
 public class Lilith {
-    private static final String CHEER_PLAYLIST = "https://www.youtube.com/watch?v=FAmojODvK64&list=RDFAmojODvK64&start_radio=1";
+    //Links, change as necessary.
+    private static final String CHEER_LINK = "https://www.youtube.com/watch?v=FAmojODvK64&list=RDFAmojODvK64&start_radio=1";
     private static final String DATA_PATH = "./LilithData/lilith.txt";
+
     //Initializes Lilith.
     public static void main(String[] args) {
         System.out.println("Hello, I'm Lilith!");
@@ -30,33 +32,6 @@ public class Lilith {
             if (input.equalsIgnoreCase("bye")) {
                 System.out.println("Bye-bye! I will always be here when you need me!");
                 break;
-            } 
-            else if (input.equalsIgnoreCase("cheer")) {
-                openCheerPlaylist();
-            } 
-            else if (input.equalsIgnoreCase("yes")) {
-                System.out.println("Okay, but you'll need to convince the oven that it is not a time machine!");
-            } 
-            else if (input.equalsIgnoreCase("no")) {
-                System.out.println("Lilith is sad...");
-            } 
-            else if (input.equalsIgnoreCase("list")) {
-                if (tasklist.isEmpty()) {
-                    System.out.println("You're free!");
-                }
-                else {
-                    for (int i = 0; i < tasklist.size(); i++){
-                    System.out.println((i + 1) + ". " + tasklist.get(i));
-                    }
-                }
-            } 
-            else if (input.toLowerCase().startsWith("find ")) {
-                String keyword = input.substring(5).trim();
-                if (keyword.isEmpty()) {
-                    System.out.println("Please provide a keyword to search for!");
-                } else {
-                    findTasks(keyword, tasklist);
-                }
             }
             else {
                 handleCommand(input, tasklist, storage);
@@ -65,11 +40,11 @@ public class Lilith {
 
         scanner.close();
     }
+
     /**
      * Searches tasks containing the keyword and prints them.
      */
     private static void findTasks(String keyword, ArrayList<Task> tasklist) {
-        System.out.println("____________________________________________________________");
         System.out.println("Here are the matching tasks in your list:");
 
         int count = 0;
@@ -84,18 +59,20 @@ public class Lilith {
         if (count == 0) {
             System.out.println("No matching tasks found for \"" + keyword + "\".");
         }
-        System.out.println("____________________________________________________________");
     }
 
-    private static void openCheerPlaylist() {
-        if (!CHEER_PLAYLIST.startsWith("https://www.youtube.com/")) {
+    /**
+     *Opens the Cheer link.
+    */
+    private static void openCheerLink() {
+        if (!CHEER_LINK.startsWith("https://www.youtube.com/")) {
             System.out.println("Invalid URL. Cannot open.");
             return;
         }
 
         if (Desktop.isDesktopSupported()) {
             try {
-                Desktop.getDesktop().browse(new URI(CHEER_PLAYLIST));
+                Desktop.getDesktop().browse(new URI(CHEER_LINK));
                 System.out.println("Cheering operation, GO!");
             } 
             catch (Exception e) {
@@ -107,19 +84,54 @@ public class Lilith {
         }
     }
 
-    //User command handling.
+    /**
+     * User command handling.
+     */
     private static void handleCommand(String input, ArrayList<Task> tasklist, Storage storage) {
         try {
             input = input.toLowerCase().trim();
-            if (input.startsWith("todo ")) {
+ 
+            if (input.equals("cheer")) {
+                openCheerLink();
+            } 
+
+            else if (input.equals("yes")) {
+                System.out.println("Okay, but you'll need to convince the oven that it is not a time machine!");
+            } 
+
+            else if (input.equals("no")) {
+                System.out.println("Lilith is sad...");
+            } 
+
+            else if (input.equals("list")) {
+                if (tasklist.isEmpty()) {
+                    System.out.println("You're free!");
+                }
+                else {
+                    for (int i = 0; i < tasklist.size(); i++){
+                    System.out.println((i + 1) + ". " + tasklist.get(i));
+                    }
+                }
+            } 
+
+            else if (input.toLowerCase().startsWith("find ")) {
+                String keyword = input.substring(5).trim();
+                if (keyword.isEmpty()) {
+                    System.out.println("Include which task are you looking for!");
+                } else {
+                    findTasks(keyword, tasklist);
+                }
+            }
+
+            else if (input.startsWith("todo ")) {
                 Task task = new Task(input.substring(5), null, null);
                 task.setTask(Task.TaskType.ToDos);
                 tasklist.add(task);
                 storage.saveTasks(tasklist);
                 System.out.println("Got it. I've added this task:\n" + task);
             } 
+
             else if (input.startsWith("deadline ")) {
-                String[] parts = Parser.parseDeadlineInput(input);
                 String[] parts = Parser.parseDeadlineInput(input.substring(9));
                 Task task = new Task(parts[0], null, parts[1]); 
                 task.setTask(Task.TaskType.Deadline);
@@ -127,8 +139,8 @@ public class Lilith {
                 storage.saveTasks(tasklist);
                 System.out.println("Got it. I've added this task:\n" + task);
             } 
+
             else if (input.startsWith("event ")) {
-                String[] parts = Parser.parseEventInput(input);
                 String[] parts = Parser.parseEventInput(input.substring(6));
                 Task task = new Task(parts[0], parts[1], parts[2]); 
                 task.setTask(Task.TaskType.Events);
@@ -136,35 +148,43 @@ public class Lilith {
                 storage.saveTasks(tasklist);
                 System.out.println("Got it. I've added this task:\n" + task);
             } 
+
             else if (input.startsWith("mark ")) {
                 int index = Integer.parseInt(input.substring(5)) - 1;
                 tasklist.get(index).mark();
                 storage.saveTasks(tasklist);
                 System.out.println("Nicely done! Good job!\n" + tasklist.get(index));
             } 
+
             else if (input.startsWith("unmark ")) {
                 int index = Integer.parseInt(input.substring(7)) - 1;
                 tasklist.get(index).unmark();
                 storage.saveTasks(tasklist);
                 System.out.println("Make sure to finish it soon, ok?\n" + tasklist.get(index));
             } 
-            else if (input.startsWith("delete ")) {
+
+            else if (input.startsWith("delete ") || input.startsWith("del ") || input.startsWith("remove ")) {
                 int index = Integer.parseInt(input.substring(7)) - 1;
                 Task removed = tasklist.remove(index);
                 storage.saveTasks(tasklist);
                 System.out.println("Ta-da! I have removed the task:\n" + removed);
             } 
+
             else if (input.equals("/emptyall")) {
                 tasklist.clear();
                 storage.saveTasks(tasklist);
                 System.out.println("All tasks have been cleared!");
             }
+
             else {
                 System.out.println("Lilith cannot find the task type...");
             }
-        } catch (IndexOutOfBoundsException e) {
+
+        }
+        catch (IndexOutOfBoundsException e) {
             System.out.println("That task does not exist!");
-        } catch (Exception e) {
+        } 
+        catch (Exception e) {
             System.out.println("Invalid input. Check format or use /by or /from /to for deadlines/events.");
         }
     }
